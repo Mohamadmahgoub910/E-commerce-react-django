82ec21e65e11b4e862f5b7f6ecc5b3381ee923c6
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-12-14 19:39:07 +0200

    fetch data api and render in react

--- base/views.py
+++ base/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+
+@api_view(['Get'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['Get'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+    return Response(product)
