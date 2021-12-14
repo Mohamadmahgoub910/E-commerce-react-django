59d9ea4c93ea5a8eb0cf0350d04a3263793543a9
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-12-14 22:55:10 +0200

    serializer models classes and make api

--- base/views.py
+++ base/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
-from .products import products
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .products import products
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
 
 
 @api_view(['Get'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
 
 @api_view(['Get'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
