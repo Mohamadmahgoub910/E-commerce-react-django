9ee77d1b280fbfdbb8dd64b6bf8e8f99ec74e34a
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-12-16 15:30:52 +0200

    clens up for som urls and views

---
+++ base/views/product_views.py
@@ -0,0 +1,22 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import Product
+from base.serializers import ProductSerializer
+# password
+
+
+# Products
+
+
+@api_view(['Get'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['Get'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
--- base/views/user_views.py
+++ base/views/user_views.py
@@ -1,16 +1,10 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.utils import serializer_helpers
-
-from .models import Product
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
-
+from base.serializers import UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 # password
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
@@ -70,20 +64,4 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-# Products
-
-
-@api_view(['Get'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['Get'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
