cbed51755669ac85a6b5af4d60f97d5cb20cc574
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-12-24 22:50:22 +0200

    handle create product for admin panel to feel comforatbale :))

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from base.models import Product
@@ -13,12 +14,48 @@ def getProducts(request):
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
+
 # product by id
-
-
 @api_view(['Get'])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+# Create Product
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name='Sample Name',
+        price=0,
+        brand='Sample Brand',
+        countInStock=0,
+        category='sample Category',
+        description=''
+    )
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+# update Product
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    # get data from frontEnd
+    data = request.data
+    product = Product.objects.get(_id=pk)
+    # get data from front end and update
+    product.name = data['name']
+    product.price = data['price']
+    product.brand = data['brand']
+    product.countInStock = data['countInStock']
+    product.category = data['category']
+    product.description = data['description']
+    product.save()
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
 
