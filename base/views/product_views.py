59fc38cf709675d9a31e80e19cdea6f83a5f832d
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-12-24 07:15:55 +0200

    handle delete product by admin

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,18 +1,19 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from base.models import Product
 from base.serializers import ProductSerializer
+from rest_framework.permissions import IsAdminUser
 # password
 
 
 # Products
-
-
 @api_view(['Get'])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+# product by id
 
 
 @api_view(['Get'])
@@ -20,3 +21,12 @@ def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+
+# delete Product with id
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response('Product Deleted Success !')
