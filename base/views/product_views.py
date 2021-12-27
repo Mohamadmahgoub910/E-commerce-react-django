39c3b6e0af1cef7b69d06040178c7da1f9fe7d46
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-12-27 03:41:38 +0200

    Handle Paginate pages and display products in more than one page

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -4,20 +4,34 @@ from rest_framework.decorators import api_view, permission_classes
 from base.models import Product, Review
 from base.serializers import ProductSerializer
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
-# password
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Products
 @api_view(['Get'])
 def getProducts(request):
     query = request.query_params.get('keyword')
-    print('query:', query)
+    # print('query:', query)
     if query == None:
         query = ''
     # products = Product.objects.all()
     products = Product.objects.filter(name__icontains=query)
+    page = request.query_params.get('page')
+    paginator = Paginator(products, 5)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    if page == None:
+        page = 1
+    page = int(page)
+
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages})
 
 
 # product by id
